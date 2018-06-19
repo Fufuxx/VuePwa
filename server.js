7159c46079334f58c585e024c3550d18739cdf14
fbastianelli <fbastianelli@salesforce.com>
2018-06-19 10:32:18 +0200

    Server static on build for nodejs server

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-// server.js
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
 
-//Getting static folder
-app.use(express.static(__dirname + '/'));
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app = express();
 
-// app.get('*', function(req, res){
-//     res.redirect('/');
-// });
+app.use(serveStatic(__dirname + "/dist"));
 
-// Start the app by listening on the default
-// Heroku port
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
+
+console.log('server started '+ port);
