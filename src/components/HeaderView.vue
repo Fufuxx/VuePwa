3ff154e41079fc10a7dbb29dcd73a6bcab3f89ff
fbastianelli <fbastianelli@salesforce.com>
2018-06-20 12:01:37 +0200

    Bug fixes

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,38 +1,16 @@
 <template>
-  <!-- <div class="ui fixed inverted menu">
-    <div class="ui container">
-      <div class="header item">
-        <img class="logo" src="/static/img/icons/twitter.png" />
-        {{ header_text }}
-      </div>
-    </div>
-  </div> -->
-  <!-- <div class="ui inverted vertical masthead center aligned segment">
-    <div class="ui container">
-      <div class="ui large secondary inverted pointing menu">
-        <a class="toc item">
-          <i class="sidebar icon"></i>
-        </a>
-        <a class="active item">Home</a>
-        <a class="item">Route</a>
-      </div>
-    </div>
-  </div> -->
   <nav class="navbar navbar-expand-lg navbar-dark">
     <router-link @click.native="sethome()"  to="/" class="navbar-brand" tag="a">Cryptocus</router-link>
     <button id="hamburger" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div id="collapsible" class="collapse navbar-collapse">
       <ul class="navbar-nav mr-auto">
         <router-link v-for="item in menu_items" :key="item.name" v-bind:class="{ 'active': item.isActive, 'nav-item': true }"
           tag="li" :to="item.url" @click.native="menuclick(item)">
           <a class="nav-link">{{ item.name }}</a>
         </router-link>
-        <!-- <router-link class="nav-item" tag="li" to="/search">
-          <a class="nav-link">Search</a>
-        </router-link> -->
       </ul>
     </div>
   </nav>
@@ -51,7 +29,9 @@ export default {
   },
   methods: {
     menuclick: function (item) {
-      document.getElementById('hamburger').click()
+      if (document.getElementById('collapsible').classList.contains('show')) {
+        document.getElementById('collapsible').classList.remove('show')
+      }
       this.$data.menu_items.forEach(function (e) { e.isActive = false })
       item.isActive = true
     },
